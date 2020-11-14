64ba0eb8c713beb124f502d415db4fa9e0bf1196
Andrew Jensen <AJensen@student.neumont.edu>
2020-11-14 03:13:09 -0700

    Testing release mode

--- Source/Raytracer/Raytracer/kernel.cu
+++ Source/Raytracer/Raytracer/kernel.cu
@@ -24,7 +24,7 @@ int main(int argc, char** argv)
     else 
     {
         //for debugging
-        rt = new Raytracer("C:\\Users\\Student\\OneDrive - Neumont College of Computer Science\\Q9 FALL 2020\\Capstone Project\\CapstoneWork\\Source\\Content\\Scenes\\scene.txt", 
+        rt = new Raytracer("C:\\Users\\Student\\OneDrive - Neumont College of Computer Science\\Q9 FALL 2020\\Capstone Project\\CapstoneWork\\Source\\Content\\Scenes\\suzanne_box.txt", 
                                   "C:\\Users\\Student\\OneDrive - Neumont College of Computer Science\\Q9 FALL 2020\\Capstone Project\\CapstoneWork\\Source\\Raytracer\\Outputs\\output.ppm");
     }
 
